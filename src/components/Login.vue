20709189830fcad5ec20a6bac881604c838a5adf
seans-23 <seanspringmann@comcast.net>
2021-06-22 20:48:33 -0400

    hey

---
+++ src/components/Login.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-card>
+        <v-card-text>
+            Hey there playa
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import Vuex from 'vuex'
+
+export default new Vuex.Store({
+    state: {
+      user: null,
+      auth: {}
+    },
+    getters: {
+      user: state => state.user,
+      auth: state => state.auth
+    },
+    mutations: {
+      user (state, value) {
+        state.user = value
+      },
+      auth (state, value) {
+        state.auth = value
+      }
+    }
+  })
